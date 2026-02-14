e72f0dffe3884aca0e1899595bfa3a527275e9e6
LaMiller0000 <160054780+LaMiller0000@users.noreply.github.com>
2026-02-13 20:25:52 -0500

    Create BuddyClass.cs
    created a basic class to describe the buddy in the todo buddy.

---
+++ todo-buddy/BuddyClass.cs
@@ -0,0 +1,45 @@
+namespace TODOBuddy;
+
+public class BuddyClass
+{
+    private int id;
+    private string name;
+    private string description;
+    private int health;
+    
+    public BuddyClass(int id) =>
+    (this.id, name, description, health) = (id, "John", "A TODO Buddy", 10);
+    
+    public BuddyClass(int id, string name, string description, int health) =>
+    (this.id, this.name, this.description, this.health) = (id, name, description, health);
+    
+    public int getId() => id;
+    public string getName() => name;
+    public string getDescription() => description;
+    public int getHealth() => health;
+    public void setId(int id) => this.id = id;
+    public void setName(string name) => this.name = name;
+    public void setDescription(string description) => this.description = description;
+    public void setHealth(int health) => this.health = health;
+
+    public void getHurt(int damage = 0)
+    {
+        if (this.health > 0)
+        {
+            this.health -= damage;
+        }
+    }
+    
+    public void eat(int food = 0) // would describe what happens when eat is called such as an eating animation
+    {
+        if (this.health > 0)
+        {
+            this.health += food;
+        }
+    }
+
+    public void death() // would describe what happens when death is called such as a death animation.
+    {
+        
+    }
+}
