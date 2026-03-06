e86c2b4332f5e1c3fe8fe49f5c999756da337f4c
Gary <garyarmstrong@gmail.com>
2026-03-06 17:30:47 -0500

    Export stuff:    added a windows export preset to the project for debugging     with Visual Studio    moved the exports (apk and now exe) to the folder Exports
    improved swiping:
      now you can swipe between each tab

--- todo-buddy/MainTabContainer.cs
+++ todo-buddy/MainTabContainer.cs
@@ -1,7 +1,8 @@
 using Godot;
 using System;
+using System.Linq;
 
-[Tool]
+//[Tool]
 public partial class MainTabContainer : Container
 {
     //[Export] public int currentTab = -1;
@@ -12,9 +13,14 @@ public partial class MainTabContainer : Container
     // if the swipe goes above this threshold before it it will lock out the horizontal 
     [Export] public float VerticalSwipeSpeedTabChangeLockout = 10.0f;
 
+    private Vector2 _defaultPosition = Vector2.Zero;
+
     private SwipeDirection _swipeDirection = SwipeDirection.None;
 
     private float _screenSwipePercentage = 0.0f;
+
+    //private Godot.Collections.Array<Node> children;
+    private Control[] _children;
 
     private enum SwipeDirection
     {
@@ -23,13 +29,59 @@ public partial class MainTabContainer : Container
         Vertical = 2,
     }
 
+    public override void _Ready()
+    {
+        _defaultPosition = Position;
+    }
+
     public override void _Notification(int what)
     {
+        //GD.Print($"_Notification: {what}");
+
+        if (what == NotificationTransformChanged && Engine.IsEditorHint())
+        {
+            _defaultPosition = Position;
+            //GD.Print("NotificationTransformChanged");
+        }
+
         if (what == NotificationSortChildren)
         {
-            GD.Print($"NotificationSortChildren");
+            //GD.Print($"NotificationSortChildren");
+
+            _children = GetChildren().OfType<Control>().ToArray<Control>();
+
+            foreach (Control child in _children)
+            {
+                //GD.Print($"  -  {child.Name}");
+                FitChildInRect(child,
+                    new Rect2(x: 0, y: 0, Size));
+                child.Visible = false;
+            }
+
+            MoveNextAndPrevChildren();
+
         }
+
     }
+
+    private void MoveNextAndPrevChildren()
+    {
+        Control current = _children[0];
+        current.Visible = true;
+        current.Position = Vector2.Zero;
+
+        //move the next one to the right side
+        Control next = _children[1];
+        next.Visible = true;
+        next.Position = new Vector2(this.Size.X, 0);
+
+        // moves the last on the the left side
+        Control prev = _children[_children.Length - 1];
+        prev.Visible = true;
+        prev.Position = new Vector2(-this.Size.X, 0);
+    }
+
+    private void Display Next 
 
     public override void _Input(InputEvent @event)
     {
@@ -69,8 +121,13 @@ public partial class MainTabContainer : Container
             //GD.Print($"touchEvent: {touchEvent.Pressed}");
             if (!touchEvent.Pressed) // runs when stop swiping
             {
+                if (_swipeDirection == SwipeDirection.Horizontal)
+                {
+                    EndSwipe();
+                }
+
                 _swipeDirection = SwipeDirection.None;
-                EndSwipe();
+
             }
         }
     }
@@ -81,20 +138,39 @@ public partial class MainTabContainer : Container
 
         float positionDelta = dragEvent.ScreenRelative.X;
 
-        tabContainer.Position += new Vector2(positionDelta, 0);
+        /*_children[0].*/Position += new Vector2(positionDelta, 0);
 
-        float totalPosDelta = (0 - tabContainer.Position.X);
+        float totalPosDelta = (0 - Position.X);
+
+        totalPosDelta += dragEvent.ScreenRelative.X * 1;
 
         _screenSwipePercentage = totalPosDelta / MainScene.Instance.ViewportResolution.X;
 
-        //GD.Print($"ScreenSwipePercentage: {_screenSwipePercentage}");
+        GD.Print($"screenSwipePercentage: {_screenSwipePercentage}");
+
     }
 
     // when swiping ends this will round to whatever screen is closest
     public void EndSwipe()
     {
         //TODO: fully implement
-        tabContainer.Position = Vector2.Zero;
+        GD.Print($"screenSwipePercentage: {_screenSwipePercentage}");
+        switch (_screenSwipePercentage)
+        {
+            case > 0.5f:
+                MoveChild(_children[0], _children.Length - 1);
+                //Position = _defaultPosition - new Vector2(Size.X, 5);
+                break;
+            case < -0.5f:
+                MoveChild(_children[_children.Length - 1], 0);
+                //Position = _defaultPosition + new Vector2(Size.X, 5);
+                break;
+            default:
+                break;
+        }
+
+        //tabContainer.
+        Position = _defaultPosition;
     }
     
 }
