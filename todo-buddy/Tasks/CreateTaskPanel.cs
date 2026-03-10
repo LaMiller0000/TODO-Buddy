cc46e80e1e028ce3c947571d98ca9346b33ff2e9
Gary Armstrong <garyarmstrong100@gmail.com>
2026-03-10 18:43:10 -0400

    cleaned up CreateTaskPanel script added public methods to control the pannel with externaly buttons

--- todo-buddy/Tasks/CreateTaskPanel.cs
+++ todo-buddy/Tasks/CreateTaskPanel.cs
@@ -3,47 +3,73 @@ using System;
 
 public partial class CreateTaskPanel : Control
 {
-    // when resizing the panel will go to the closest snap pos
+    // when the size_ratio of the pannel is within this number it stops updating the size
+    public const float SNAPPING_CLOSENESS_FREEZE_THRESHOLD = 0.01f;
+
+    [Export] public bool EnableSnapping = true;
+
+    // how far the panel will move in one second
+    [Export] public float SnapHomeingSpeed = 2.0f;
+
+    // allows flicking to move the pannel
+    // this is used when finding the closest snaping point
+    [Export] public float FlickPower = 2;
+
+    [ExportGroup("Snap Points")]
+    // ***** Snap points ***** //
+    // after resizing, the panel will go to the closest snap pos
     // measured as the ratio of the parent control's size
-    //[Export] public float[] SizeSnapPos = new float[] {0.1f, 0.95f, 0.45f};
     [Export] public float ClosedPanelSizeRatio = 0.0f;
     [Export] public float HalfOpenPanelSizeRatio = 0.45f;
     [Export] public float FullOpenPanelSizeRatio = 0.95f;
-    [Export] public bool EnableSnapping = true;
-    [Export] public float SnapHomeingSpeed = 1.0f;
-    // this is used when finding the closest snaping point
-    [Export] public float FlickPower = 2; // allows flicking to move the pannel
 
-    // when the acuall size of the pannel is within this number it stops updating the size
-    public const float SNAPPING_CLOSENESS_FREEZE_THRESHOLD = 0.01f;
+    public event Action PannelClosed;
+
+    // true when the user is swiping the panel up or down
+    private bool _isResizing = false;
+    private float _prevCursorPosY_px;
+    private float cursorPosYDelta = 0;
 
     //the ratio from the bottom of the screen = 0.0f to the top of the screen = 1.0f;
     private float _targetPanelSize_Ratio = 1.0f;
 
-    private bool _isSlideResizing = false;
-    private float _prevCursorPosY_px;
-    private float cursorPosYDelta = 0;
-
-
-    private Vector2 _parentSize;
+    private Vector2 _parentSize_Px;
+    // used to convert sizeY ratio value to sizeY pixel value
     // multiply a sizeRatio by this to get it as a Size in pixels 
     private float _Ratio2Pixel_sizeY;
+    // used to convert sizeY pixel value to sizeY ratio value
+    // multiply a sizeY ratio value by this to get it as a SizeY in pixels 
     // this is the cached value for turning a size in px to a size ratio
     // its value should be: 1 / Parent.Size.Y
-    // its used like this: size.Y * _sizeY2RatioSizeY = sizeYratio
     private float _pixel2Ratio_sizeY;
     // automatically caclulates the sizeYratio using _sizeY2RatioSizeY
     private float sizeYAsRatio => this.Size.Y * _pixel2Ratio_sizeY;
 
+    public void OpenPanel()
+    {
+        this.Visible = true;
+        SetPanelPosition(HalfOpenPanelSizeRatio);
+    }
+    
+    public void SetPanelPosition(float newSize_ratio)
+    {
+        FindClosestSnapPos(newSize_ratio);
+    }
+    public void ClosePanel()
+    {
+        SetPanelPosition(ClosedPanelSizeRatio);
+        PannelClosed?.Invoke(); 
+    }
+
     public void OnSlideResize_start()
 	{
-        _isSlideResizing = true;
+        _isResizing = true;
 
         _prevCursorPosY_px = this.GetGlobalMousePosition().Y;
     }
     public void OnSlideResize_stop()
     {
-		_isSlideResizing = false;
+		_isResizing = false;
 
         if (EnableSnapping) FindClosestSnapPos(sizeYAsRatio + cursorPosYDelta * FlickPower);
     }
@@ -55,12 +81,14 @@ public partial class CreateTaskPanel : Control
         float avgPoint1 = (HalfOpenPanelSizeRatio + ClosedPanelSizeRatio) * 0.5f;
         float avgPoint2 = (FullOpenPanelSizeRatio + HalfOpenPanelSizeRatio) * 0.5f;
 
-        if (sizeYAsRatio < avgPoint1) // Set to closed
+        if (panelSizeYRatio < avgPoint1) // Set to closed
         {
             GD.Print($"Set to closed");
+
+            PannelClosed?.Invoke(); 
             _targetPanelSize_Ratio = ClosedPanelSizeRatio;
         }
-        else if (sizeYAsRatio < avgPoint2) // set to half open
+        else if (panelSizeYRatio < avgPoint2) // set to half open
         {
             GD.Print($"set to half open");
             _targetPanelSize_Ratio = HalfOpenPanelSizeRatio;
@@ -75,9 +103,9 @@ public partial class CreateTaskPanel : Control
     private void OnParentResize()
     // should run once on startup and every time the screen changes resolution
     {
-        _parentSize = GetParent<Control>().Size;
+        _parentSize_Px = GetParent<Control>().Size;
 
-        _Ratio2Pixel_sizeY = _parentSize.Y;
+        _Ratio2Pixel_sizeY = _parentSize_Px.Y;
         // caches this division so it only has to be used once
         _pixel2Ratio_sizeY = 1 / _Ratio2Pixel_sizeY;
 
@@ -94,7 +122,7 @@ public partial class CreateTaskPanel : Control
     public override void _Process(double delta) // runs every tick, its the main loop for general every frame processes
 	{
         // checks if currently resizing the panel
-        if (_isSlideResizing)
+        if (_isResizing)
         {
             // calculates distance the cursor traveled between this and the last frame
             float cursorPosY_px = this.GetGlobalMousePosition().Y;
@@ -115,42 +143,8 @@ public partial class CreateTaskPanel : Control
                 // homes the size towards the closest snap position
                 Vector2 sizeDelta_Px = new Vector2(this.Size.X, sizeDelta_Ratio * _Ratio2Pixel_sizeY);
                 this.Size = sizeDelta_Px;
-                this.Position = _parentSize - sizeDelta_Px;
-                
+                this.Position = _parentSize_Px - sizeDelta_Px;
             }
         }
     }
 }
-
-
-/*
-
-GD.Print(
-    $"_targetPanelSize_Ratio:{_targetPanelSize_Ratio},                      ",
-    $"sizeYAsRatio:{sizeYAsRatio},                                          ",
-    $"sizeDelta_Ratio:{sizeDelta_Ratio},                                    ",
-    $"sizeDelta_Px:{sizeDelta_Px.Y},                                        ",
-    $"this.Size.Y:{this.Size.Y},                                            ",
-    $"this.Pos.Y:{this.Position.Y},                                         "
-); //TODO:Remove this
-
-
-if (MathF.Abs(sizeYAsRatio - _targetPanelSize_Ratio) > SNAPPING_CLOSENESS_FREEZE_THRESHOLD)
-            {
-                float sizeDelta_Ratio = Mathf.MoveToward(sizeYAsRatio, _targetPanelSize_Ratio, SnapHomeingSpeed);
-
-                // homes the size towards the closest snap position
-                Vector2 sizeDelta_Px = new Vector2(this.Size.X, sizeDelta_Ratio * _Ratio2Pixel_sizeY); // (0, (this.Size.Y - _closestSnapPos) / SnapHomeingSpeed);
-                this.Size = sizeDelta_Px;
-                this.Position = new Vector2(GetParent<Control>().Size.X, GetParent<Control>().Size.Y) - sizeDelta_Px;
-                GD.Print(
-                    $"_targetPanelSize_Ratio:{_targetPanelSize_Ratio},                      ",
-                    $"sizeYAsRatio:{sizeYAsRatio},                                          ",
-                    $"sizeDelta_Ratio:{sizeDelta_Ratio},                                    ",
-                    $"sizeDelta_Px:{sizeDelta_Px.Y},                                        ",
-                    $"this.Size.Y:{this.Size.Y},                                            ",
-                    $"this.Pos.Y:{this.Position.Y},                                         "
-                ); //TODO:Remove this
-            }
-            else GD.Print("CreateTaskPanel.cs: Done Moveing to snap position"); //TODO:Remove this 
-*/
