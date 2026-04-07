ba3f0f6ebab50b2988db69ab261712e762bdaaa9
Gary <garyarmstrong@gmail.com>
2026-04-07 15:44:23 -0400

    added sorting functionality to the Task Display List

--- todo-buddy/Tasks/TaskListTab.cs
+++ todo-buddy/Tasks/TaskListTab.cs
@@ -4,20 +4,25 @@ using System;
 public partial class TaskListTab : Control
 {
 	[Export] public CreateTaskPanel CreateTaskPanel;
-	public override void _Ready()
+	[Export] public TaskDisplayList TaskDisplayList;
+
+    public override void _Ready()
 	{
 		CreateTaskPanel.ClosePanel();
 	}
 	public void OnFilterChanged(int index)
 	{
-		// pass this directly to the script handling TaskDisplayList
-		throw new NotImplementedException("tried to run OnFilterChanged() which is not implemented yet");
+		TaskDisplayList.SortOptions filterOptions = (TaskDisplayList.SortOptions)index;
+
+        TaskDisplayList.SortOption = filterOptions;
+
+        TaskDisplayList.Refresh();
 	}
 	public void OnCreateButtonPressed()
 	{
 		// pull up for creating new tasks
-
 		CreateTaskPanel.OpenPanel();
-		//throw new NotImplementedException("tried to run OnCreateButtonPressed() which is not implemented yet");
 	}
+
+	
 }
