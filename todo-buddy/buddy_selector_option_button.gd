4ce755338ef660d59ac90e00ded0beb4ed1c91a2
Emma Raymond <ecraymond1@gmail.com>
2026-04-10 10:55:03 -0400

    Fucking finally
    Lesson learned for this week, try putting code in places since comments aren't recognized.

--- todo-buddy/buddy_selector_option_button.gd
+++ todo-buddy/buddy_selector_option_button.gd
@@ -11,5 +11,12 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_item_selected(index: int) -> void:
-	pass # Replace with function body.
+func _on_item_selected(index: int) -> void: #ER, On Item Selected in Drop Down
+	match index:
+		0: print(index) #blue hero
+		1: print(index) #blue slime
+		2: print(index) #gold slime
+		3: print(index) #green slime
+		4: print(index) #red demon
+		_: print(index) #default
+	pass
