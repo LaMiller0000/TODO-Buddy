f40ae6f34a48b1b652bbe1bc6bde6fee6b569bb0
Emma Raymond <ecraymond1@gmail.com>
2026-04-14 10:38:59 -0400

    Adding onto the interactions class

--- todo-buddy/buddy_animated_sprite.gd
+++ todo-buddy/buddy_animated_sprite.gd
@@ -1,5 +1,24 @@
 extends AnimatedSprite2D
 
+@onready var _buddy = $AnimatedSprite2D;
+
+enum buddySkinOpts{
+	defaultDead,
+	defaultIdle,
+	defaultSleep,
+	evolvedDead,
+	evolvedIdle,
+	evolvedSleep,
+}
+
+enum buddySelectedOpts{
+	blueHero, 
+	blueSlime, 
+	goldSlime, 
+	orangeSlime, 
+	redDemon, 
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#BuddySkin = buddy_selector_option_button
@@ -14,17 +33,27 @@ func _process(delta: float) -> void:
 #changes animation
 func _change_animation(newAnimation: String) -> void:
 	
+	match newAnimation:
+		0: _buddy.play("defaultDead");
+		1: _buddy.play("defaultIdle");
+		2: _buddy.play("defaultSleep");
+		3: _buddy.play("defaultDead");
+		4: _buddy.play("defaultIdle");
+		5: _buddy.play("defaultSleep");
+		_: print(newAnimation) #default
+	
 	print(newAnimation);
 	pass
 	
 #changes sprite set
 func _change_sprite_set(newSprite: int) -> void:
 	match newSprite:
-		0: AnimatedSprite2D.play("") #blue hero
-		1: print(newSprite) #blue slime
-		2: print(newSprite) #gold slime
-		3: print(newSprite) #green slime
-		4: print(newSprite) #red demon
+		0: _buddy.play();
+		1: _buddy.play();
+		2: _buddy.play();
+		3: _buddy.play();
+		4: _buddy.play();
 		_: print(newSprite) #default
+		
 	print(newSprite);
 	pass
