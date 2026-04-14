c3d20e12e0a61c43812b7511975b98b37d6b84f9
Emma Raymond <ecraymond1@gmail.com>
2026-04-14 10:18:14 -0400

    Making Animation class

---
+++ todo-buddy/BuddyPageInteractionsClass.cs
@@ -0,0 +1,26 @@
+using System;
+using Godot;
+
+public BuddyPageInteractions : public BuddyClass, OptionButton, AnimatedSprite2D {
+	static var BuddySkin: string;
+	static var BuddySelected: string;
+	
+	BuddyPageInteractions(){ //Constructor
+		BuddySkin = AnimatedSprite2D._on_item_selected();
+	}
+	
+	string getBuddySkin(){
+		return BuddySkin;
+	}
+	string getBuddySelected(){
+		return BuddySelected;
+	}
+	
+	string setBuddySkin(){
+		return BuddySkin;
+	}
+	string setBuddySelected(){
+		return BuddySelected;
+	}
+	
+}
--- todo-buddy/buddy_animated_sprite.gd
+++ todo-buddy/buddy_animated_sprite.gd
@@ -1,15 +1,30 @@
 extends AnimatedSprite2D
-static var BuddySkin;
-static var BuddySelected;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#BuddySkin = buddy_selector_option_button
 	#BuddySelected = buddy_selector_option_button
-	
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+	
+#changes animation
+func _change_animation(newAnimation: String) -> void:
+	
+	print(newAnimation);
+	pass
+	
+#changes sprite set
+func _change_sprite_set(newSprite: int) -> void:
+	match newSprite:
+		0: AnimatedSprite2D.play("") #blue hero
+		1: print(newSprite) #blue slime
+		2: print(newSprite) #gold slime
+		3: print(newSprite) #green slime
+		4: print(newSprite) #red demon
+		_: print(newSprite) #default
+	print(newSprite);
 	pass
